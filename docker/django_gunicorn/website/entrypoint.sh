cf4c68ca621d3b0b1db3989fe17611639ee0ec5e
Febriands <febrian.d.s@gmail.com>
2022-01-17 15:37:58 +0700

    first commit

---
+++ docker/django_gunicorn/website/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+python manage.py makemigrations --no-input
+python manage.py migrate --no-input
+python manage.py collectstatic --no-input
+
+DJANGO_SUPERUSER_USERNAME=admin \
+DJANGO_SUPERUSER_PASSWORD=admin \
+DJANGO_SUPERUSER_EMAIL=admin@admin.com \
+python manage.py createsuperuser --noinput
+
+gunicorn website.wsgi:application --bind 0.0.0.0:8000
